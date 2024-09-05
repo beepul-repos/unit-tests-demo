4ed794f081155beef6544f7c486b531f647b0af5
AMazhidov <AMazhidov@beelab.uz>
2024-09-05 11:09:25 +0500

    Push beephelper

---
+++ src/Application/Helpers/BeepHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Helpers;
+
+public static class BeepHelper
+{
+    public static int GetCalculatedBeeps(decimal paymentAmount, decimal beepRate, int beepRateFactor)
+    {
+        return (int) Math.Truncate(paymentAmount / beepRate) * beepRateFactor;
+    }
+}
